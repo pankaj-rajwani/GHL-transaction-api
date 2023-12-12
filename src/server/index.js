1ea094088b1920e3fad96b535bf00ea4242e0667
Pankaj Rajwani <rajwanipankaj23@gmail.com>
2023-12-12 18:39:14 +0530

    update: removes obsolete console.log statement

--- src/server/index.js
+++ src/server/index.js
@@ -16,7 +16,6 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 routes(app);
-console.log(process.env.MONGODB_URI);
 
 mongoose.connect(
   process.env.MONGODB_URI ||
